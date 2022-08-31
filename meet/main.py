11a1c0c551891450fe2cbf9445e06eea263e09d8
alfred <alfred.chen@104.com.tw>
2022-08-31 14:53:06 +0800

    option: outputcsv

--- meet/main.py
+++ meet/main.py
@@ -14,5 +14,5 @@ load_dotenv()
 username, password = os.environ.get('eportal_username'),os.environ.get('eportal_password')
 
 session = eportal_util.get_login_session(username, password)
-data = meet_crawler.get_meets_reserve(session, '張志宏', 4)
+data = meet_crawler.get_meets_reserve(session, '張志宏', 4, output_csv=True)
 print(data)
